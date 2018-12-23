260da2002322563c59d2376f55e496c849b67281
Serge Shkurko <shkurko1998@gmail.com>
2018-12-23 20:45:12 +0500

    Add docs (#1)

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const arg = (argList => {
     argList.forEach((item, index) => {
       thisOpt = item.trim()
       opt = thisOpt.replace(/^\-+/, '')
-  
+
       if (opt === thisOpt) {
         // argument value
         if (curOpt) arg[curOpt] = opt
@@ -18,34 +18,38 @@ const arg = (argList => {
         // argument name
         curOpt = opt
         arg[curOpt] = true
-  
+
       }
     })
 
   return arg;
 })(process.argv)
 
-const typeScriptProject = typeScript.createProject('tsconfig.json', { 
-    target: arg.target || 'es6', 
+const typeScriptProject = typeScript.createProject('tsconfig.json', {
+    target: arg.target || 'es6',
     module: arg.module || 'es2015'
   }),
   typeScriptProjectTest = typeScript.createProject('./test/tsconfig.json')
 
-gulp.task('build', () => 
-  typeScriptProject
+function build() {
+  return typeScriptProject
     .src()
     .pipe(typeScriptProject())
     .pipe(gulp.dest(`lib/${arg.module}`))
-)
+}
 
-gulp.task('build:test', () =>
-  gulp
+function buildTests() {
+  return gulp
     .src(['test/src/**/*.ts'])
     .pipe(typeScriptProjectTest())
     .pipe(gulp.dest(`test/build`))
-)
+}
 
-gulp.task('watching', ['build:test', 'build'], () => {
-  gulp.watch(['test/src/**/*.ts'], ['build:test'])
-  gulp.watch(['src/**/*.ts', 'src/**/*.js'], ['build'])
+gulp.task('build', build)
+
+gulp.task('build:test', buildTests)
+
+gulp.task('watching', () => {
+  gulp.watch('test/src/**/*.ts', buildTests)
+  gulp.watch(['src/**/*.ts', 'src/**/*.js'], build)
 })
